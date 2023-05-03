073487147ac55e23042dc947fbea9e217f404866
Koray <74928882+koraydincc@users.noreply.github.com>
2023-05-03 17:37:49 +0300

    Add files via upload

--- example2/ödev-2/mathexample.js
+++ example2/ödev-2/mathexample.js
@@ -1,11 +1,16 @@
 
-
-
 document.getElementById('btn-topla').addEventListener('click', function(){
 
     let val1 = document.getElementById('inp-no').value;
     let val2 = document.getElementById('inp-no-iki').value;
 
+    if (val1 === '') 
+    {
+       alert('Tüm Alanları Doldurunuz!');
+       return;   
+    }
+
+   
     let mutlakDeger = Math.abs(val1) + Math.abs(val2);
 
     document.getElementById('result').innerHTML = `Sonuç = ${mutlakDeger}`;
@@ -18,6 +23,12 @@ document.getElementById('btn-cikar').addEventListener('click', function(){
 
     let val1 = document.getElementById('inp-no').value;
     let val2 = document.getElementById('inp-no-iki').value;
+
+    if (val1 === '') 
+    {
+       alert('Tüm Alanları Doldurunuz!');
+       return;   
+    }
 
     let cikar = val1 - val2;
 
@@ -32,6 +43,12 @@ document.getElementById('btn-bol').addEventListener('click', function(){
     let val1 = document.getElementById('inp-no').value;
     let val2 = document.getElementById('inp-no-iki').value;
 
+    if (val1 === '') 
+    {
+       alert('Tüm Alanları Doldurunuz!');
+       return;   
+    }
+
     let bol = val1 / val2;
 
     document.getElementById('result').innerHTML = `Sonuç = ${bol}`
@@ -45,6 +62,12 @@ document.getElementById('btn-carp').addEventListener('click', function(){
     let val1 = document.getElementById('inp-no').value;
     let val2 = document.getElementById('inp-no-iki').value;
 
+    if (val1 === '') 
+    {
+       alert('Tüm Alanları Doldurunuz!');
+       return;   
+    }
+
     let carp = val1 * val2;
 
     document.getElementById('result').innerHTML = `Sonuç = ${carp}`
@@ -54,12 +77,26 @@ document.getElementById('btn-carp').addEventListener('click', function(){
 })
 
 document.getElementById('btn-random').addEventListener('click', function() {
+
     let val1 = parseInt(document.getElementById('inp-no').value);
+
+
     let val2 = Math.floor(Math.random() * 10); 
+
     let sonuc = val1 * val2 + val1;
 
     document.getElementById('result').innerHTML = `Sonuç = ${sonuc}, Random Sayınız = ${val2}`;
 });
+
+document.getElementById('btn-kok').addEventListener('click', function(){
+
+    let val1 = Math.floor(Math.sqrt(document.getElementById('inp-no').value));
+    
+    document.getElementById('result').innerHTML = `Sonuç = ${val1}`
+
+
+
+})
 
 
 document.getElementById('clear').addEventListener('click', function(){
